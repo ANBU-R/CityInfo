26d210e897a4ed6afa193d676ede3e7a9dfafc20
Anbarasan R <51128356+ANBU-R@users.noreply.github.com>
2024-03-14 16:40:10 +0530

    Add status code to controller

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -1,18 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CityInfo.API.Model;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Controllers
 {
     [ApiController]
     [Route("api/cities")]
-    public class CitiesController:ControllerBase
+    public class CitiesController : ControllerBase
     {
         [HttpGet]
-        public JsonResult GetCities()
+        public ActionResult<IEnumerable<CityDto>> GetCities()
         {
-            return new JsonResult(
-                CitiesDataStore.Current.Cities
+            return Ok(CitiesDataStore.Current.Cities);
+        }
 
-                );
+        [HttpGet("{id}")]
+
+
+        public ActionResult<CityDto> GetCity(int id)
+        {
+
+            var response = CitiesDataStore.Current.Cities.Find(city => city.Id == id);
+            if (response == null)
+                return NotFound();
+
+            return Ok(response);
+
+
         }
     }
 }
