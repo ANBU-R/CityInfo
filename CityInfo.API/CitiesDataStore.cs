920b521b8d41815a99fca53cd14e540d5bbaadef
Anbarasan R <51128356+ANBU-R@users.noreply.github.com>
2024-03-14 12:16:13 +0530

    Create dummy  data  store for city

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,19 @@
+﻿using CityInfo.API.Model;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current {get;} =  new CitiesDataStore();
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>() { 
+                new CityDto() { Id = 1, Name = "Chennai", Description = "A conservative south indian city" }, 
+                new CityDto() { Id = 2, Name="NYC", Description="The One with crazy people"},
+                new CityDto() { Id = 3, Name="Paris", Description="The One with big tower"}
+            };
+        }
+
+        public List<CityDto> Cities { get; }
+    }
+}
