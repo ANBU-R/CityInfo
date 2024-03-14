5644c832682911883fe73fd9a26ae13a693dcbf3
Anbarasan R <51128356+ANBU-R@users.noreply.github.com>
2024-03-14 12:01:00 +0530

    Add first route for city

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController:ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new object[]
+                {
+                    new {id=1, name="Chennai"}
+                    , new {id=2, name="Madurai"}
+
+                }
+
+                );
+        }
+    }
+}
--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -18,9 +18,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseRouting();
 app.UseAuthorization();
-
-app.MapControllers();
+app.UseEndpoints(endpoints =>endpoints.MapControllers());
+//app.MapControllers();
 
 app.Run();
