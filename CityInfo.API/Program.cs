bd5dbae9d7225d03c48f4c8bcd9ec39ec3aa4a04
Anbarasan R <51128356+ANBU-R@users.noreply.github.com>
2024-03-15 15:20:03 +0530

    Add 406 error handling for unsupported Accept types in controller services configuration

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -2,7 +2,23 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(
+    options =>
+    {
+        //return 406 error message if Accept type is not supported
+        options.ReturnHttpNotAcceptable = true;
+    });
+
+//service to manipulate custom error details
+//builder.Services.AddProblemDetails((options) =>
+//{
+//    options.CustomizeProblemDetails = context =>
+//    {
+//        context.ProblemDetails.Extensions.Add("Additional dummy info", "example info");
+//        context.ProblemDetails.Extensions.Add("server", Environment.MachineName);
+//    };
+//});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -20,7 +36,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseRouting();
 app.UseAuthorization();
-app.UseEndpoints(endpoints =>endpoints.MapControllers());
+app.UseEndpoints(endpoints => endpoints.MapControllers());
 //app.MapControllers();
 
 app.Run();
