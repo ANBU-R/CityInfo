7debd9a6bf84ee075b85b67761686ff415e4d5ea
Anbarasan R <51128356+ANBU-R@users.noreply.github.com>
2024-03-15 14:52:11 +0530

    Add PointsOfInterest child resources to the response

--- CityInfo.API/CitiesDataStore.cs
+++ CityInfo.API/CitiesDataStore.cs
@@ -4,13 +4,51 @@ namespace CityInfo.API
 {
     public class CitiesDataStore
     {
-        public static CitiesDataStore Current {get;} =  new CitiesDataStore();
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
         public CitiesDataStore()
         {
-            Cities = new List<CityDto>() { 
-                new CityDto() { Id = 1, Name = "Chennai", Description = "A conservative south indian city" }, 
-                new CityDto() { Id = 2, Name="NYC", Description="The One with crazy people"},
-                new CityDto() { Id = 3, Name="Paris", Description="The One with big tower"}
+            Cities = new List<CityDto>() {
+                new CityDto() { Id = 1, Name = "Chennai", Description = "A conservative south indian city",
+                    PointsOfInterest=new List<PointOfInterestDto>(){
+                    new PointOfInterestDto()
+                    {
+                        Id = 1,
+                        Name="Beach"
+                    },
+                    new PointOfInterestDto()
+                    {
+                        Id=2,
+                        Name="Museum"
+                    }
+                } },
+                new CityDto() { Id = 2, Name="NYC", Description="The One with crazy people",
+                PointsOfInterest= new List<PointOfInterestDto>()
+                {
+                    new PointOfInterestDto()
+                    {
+                        Id=3,
+                        Name="Central Park"
+                    },
+                    new PointOfInterestDto()
+                    {
+                        Id=4,
+                        Name="Empire state building"
+                    }
+                }
+
+
+                },
+                new CityDto() { Id = 3, Name="Paris",
+                    Description="The One with big tower",
+                    PointsOfInterest= new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto(){
+                        Id=5,
+                        Name="Eiffel Tower"
+                        }
+                    }
+
+                }
             };
         }
 
---
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,41 @@
+﻿using CityInfo.API.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities/{cityId}/pointsofinterest")]
+    [ApiController]
+    public class PointsOfInterestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<PointOfInterestDto>> GetPointsOfInterest(int cityId)
+        {
+            var city = CitiesDataStore.Current.Cities.Find(city => city.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(city.PointsOfInterest);
+
+        }
+
+        [HttpGet("{interestId}")]
+        public ActionResult<PointOfInterestDto> getPointOfInterst(int cityId, int interestId
+            )
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(city => city.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+            var pointOfInterest = city.PointsOfInterest.FirstOrDefault(p => p.Id == interestId);
+            if (pointOfInterest == null)
+            {
+                return NotFound();
+            }
+            return Ok(pointOfInterest);
+
+        }
+    }
+}
