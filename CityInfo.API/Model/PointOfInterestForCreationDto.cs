6858e58d8b8668f5e8c767b7a788736ea98c0055
Anbarasan R <51128356+ANBU-R@users.noreply.github.com>
2024-03-16 13:54:58 +0530

    Add Validation rule

--- CityInfo.API/Model/PointOfInterestForCreationDto.cs
+++ CityInfo.API/Model/PointOfInterestForCreationDto.cs
@@ -1,14 +1,16 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace CityInfo.API.Model
 {
     public class PointOfInterestForCreationDto
     {
 
-
+        [Required]
+        //custom error message with ErrirMessage arqument
+        [MaxLength(50, ErrorMessage = "Must be less than 50 char length")]
         public string Name { get; set; } = string.Empty;
 
-
+        [MaxLength(200)]
         public string? Description { get; set; }
     }
 }
