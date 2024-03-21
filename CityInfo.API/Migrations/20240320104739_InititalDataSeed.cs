eadd72ba282a7363e3bdec5a21cebf3a54650078
Anbarasan R <51128356+ANBU-R@users.noreply.github.com>
2024-03-21 18:21:10 +0530

    Add db migration

---
+++ CityInfo.API/Migrations/20240320104739_InititalDataSeed.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CityInfo.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InititalDataSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "The one with big bark", "New York" },
+                    { 2, "The one with  beach", "Cennai" },
+                    { 3, "The one with pointy tower", "Paris" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "PointOfInterests",
+                columns: new[] { "Id", "CityId", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "The wild park in the middle of the city", "Central Park" },
+                    { 2, 1, "A skyscraper in manhatten", "Empire State Building" },
+                    { 3, 2, "longest beach in india", "Marina beach" },
+                    { 4, 2, "Has lots of historical items", "Egmore Museum" },
+                    { 5, 3, null, "Eiffel Tower" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
