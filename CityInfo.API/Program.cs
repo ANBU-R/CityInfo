e98805c50e2b3f7685c73c7de0f485119fac3dae
Anbarasan R <51128356+ANBU-R@users.noreply.github.com>
2024-03-21 18:20:42 +0530

    Added SQLite database connection to CityInfoContext

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,6 +1,8 @@
 
 using CityInfo.API;
+using CityInfo.API.DbContexts;
 using CityInfo.API.Services;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 //configure serilog
@@ -58,6 +60,8 @@ builder.Services.AddTransient<IMailService, CloudMailService>();
 #endif
 
 builder.Services.AddSingleton<CitiesDataStore>();
+builder.Services.AddDbContext<CityInfoContext>(
+   (dbContextOptions) => dbContextOptions.UseSqlite(builder.Configuration["ConnectionStrings:CityInfoDBConnectionString"]));
 
 var app = builder.Build();
 
