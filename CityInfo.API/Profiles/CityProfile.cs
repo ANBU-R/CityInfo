26d02533aa1a2fe3cff0bef731b10fbed28b91d5
Anbarasan R <51128356+ANBU-R@users.noreply.github.com>
2024-03-23 13:22:51 +0530

    Add Profile for AutoMapper

---
+++ CityInfo.API/Profiles/CityProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using CityInfo.API.Entities;
+using CityInfo.API.Model;
+
+namespace CityInfo.API.Profiles
+{
+    public class CityProfile : Profile
+    {
+        // Defines a mapping profile for AutoMapper
+        // to map properties from Entities.City to Model.CityWithoutPointOfInterestDto
+        public CityProfile()
+        {
+            CreateMap<City, CityWithoutPointOfInterestDto>();
+            CreateMap<City, CityDto>();
+        }
+
+    }
+}
---
+++ CityInfo.API/Profiles/PointOfInterestProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CityInfo.API.Entities;
+using CityInfo.API.Model;
+
+namespace CityInfo.API.Profiles
+{
+    public class PointOfInterestProfile : Profile
+    {
+        public PointOfInterestProfile()
+        {
+            CreateMap<PointOfInterest, PointOfInterestDto>();
+            CreateMap<PointOfInterestForCreationDto, PointOfInterest>();
+        }
+    }
+}
