4ad1d1f9538b4c9da647d3adbbbb3ce96797b7aa
Anbarasan R <51128356+ANBU-R@users.noreply.github.com>
2024-03-23 13:15:30 +0530

    Add documentation comments

--- CityInfo.API/Entities/City.cs
+++ CityInfo.API/Entities/City.cs
@@ -1,32 +1,47 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations; // Importing the namespace for data annotations.
+using System.ComponentModel.DataAnnotations.Schema; // Importing the namespace for schema annotations.
 
 namespace CityInfo.API.Entities
 {
+    /// <summary>
+    /// Represents a city entity.
+    /// </summary>
     public class City
     {
-
-        //set id as a primary key
+        /// <summary>
+        /// Gets or sets the ID of the city (primary key).
+        /// </summary>
         [Key]
         [Required]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-
         public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the city.
+        /// </summary>
         [Required]
         [MaxLength(50)]
         public string Name { get; set; }
-        [MaxLength(200)]
 
+        /// <summary>
+        /// Gets or sets the description of the city.
+        /// </summary>
+        [MaxLength(200)]
         public string? Description { get; set; }
 
+        /// <summary>
+        /// Gets or sets the collection of points of interest associated with the city.
+        /// </summary>
         public ICollection<PointOfInterest> PointsOfInterest { get; set; }
-        = new List<PointOfInterest>();
+            = new List<PointOfInterest>();
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="City"/> class with the specified name.
+        /// </summary>
+        /// <param name="name">The name of the city.</param>
         public City(string name)
         {
             Name = name;
-
         }
-
     }
 }
--- CityInfo.API/Entities/PointOfInterest.cs
+++ CityInfo.API/Entities/PointOfInterest.cs
@@ -1,32 +1,49 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations; // Importing the namespace for data annotations.
+using System.ComponentModel.DataAnnotations.Schema; // Importing the namespace for schema annotations.
 
 namespace CityInfo.API.Entities
 {
+    /// <summary>
+    /// Represents a point of interest entity.
+    /// </summary>
     public class PointOfInterest
     {
+        /// <summary>
+        /// Gets or sets the ID of the point of interest (primary key).
+        /// </summary>
         [Key]
         [Required]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
-
+        /// <summary>
+        /// Gets or sets the name of the point of interest.
+        /// </summary>
         [Required]
         [MaxLength(50)]
         public string Name { get; set; }
 
+        /// <summary>
+        /// Gets or sets the description of the point of interest.
+        /// </summary>
         [MaxLength(200)]
         public string? Description { get; set; }
 
+        /// <summary>
+        /// Gets or sets the associated city of the point of interest.
+        /// </summary>
         [ForeignKey("CityId")]
-        // navigation property
         public City? City { get; set; }
 
-        // foreign key
-
+        /// <summary>
+        /// Gets or sets the foreign key referencing the city to which the point of interest belongs.
+        /// </summary>
         public int CityId { get; set; }
 
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PointOfInterest"/> class with the specified name.
+        /// </summary>
+        /// <param name="name">The name of the point of interest.</param>
         public PointOfInterest(string name)
         {
             Name = name;
